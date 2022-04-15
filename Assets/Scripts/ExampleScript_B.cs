bea1dd3139decbefad00b59ea1e722d76e0b7d7b
Brandon Oh <brandonoh2938@gmail.com>
2022-04-15 18:47:57 -0400

    More fundamental methods added.

--- Assets/Scripts/ExampleScript_B.cs
+++ Assets/Scripts/ExampleScript_B.cs
@@ -6,6 +6,9 @@ public class ExampleScript_B : MonoBehaviour
 {
     private int enemyDistance = 0;
     private int enemyCount = 4;
+
+    private string[] enemies = new string[5];
+    private int weaponId = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,9 @@ public class ExampleScript_B : MonoBehaviour
         {
             //EnemySearch();
             //EnemeyDestruction();
-            EnemyScan();
+            //EnemyScan();
+            //EnemyRoster();
+            WeaponSearch();
         }
     }
 
@@ -62,5 +67,45 @@ public class ExampleScript_B : MonoBehaviour
             print("Scanning for enemies!");
         }
         while (isAlive == true);
+    }
+
+    void EnemyRoster()
+    {
+        enemies[0] = "Orc";
+        enemies[1] = "Dragon";
+        enemies[2] = "Goblin";
+        enemies[3] = "Golem";
+        enemies[4] = "Unicorn";
+        foreach(string enemy in enemies)
+        {
+            print(enemy);
+        }
+    }
+
+    void WeaponSearch()
+    {
+        weaponId = Random.Range(0, 7);
+
+        switch (weaponId)
+        {
+            case 1:
+                print("You found a sword!");
+                break;
+            case 2:
+                print("You found a axe!");
+                break;
+            case 3:
+                print("You found a dagger!");
+                break;
+            case 4:
+                print("You found a mace!");
+                break;
+            case 5:
+                print("You found a staff!");
+                break;
+            default:
+                print("Guess you found nothing...");
+                break;
+        }
     }
 }
